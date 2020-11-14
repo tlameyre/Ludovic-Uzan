9c56c80fcf50fa3c157aa5cb98537e8365c1c41d
Theotime Lameyre <theotime.lameyre@gmail.com>
2020-11-14 13:42:55 +0100

    Css des pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def profil
+  end
+
+  def parcours
+  end
+
+  def experience
+  end
+
+  def projet
+  end
+
+  def hobbie
+  end
 end
